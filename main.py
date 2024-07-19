1846a63fa068859fc7280dcf4df1e260b526d158
SamraAzizi <fahimaazizi369@gmail.com>
2024-07-19 20:26:07 +0500

    my commit

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ while True:
     _, img = webcam.read()
     cv2.imshow("FACE DETECTION", img)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.5)
+    faces = face_cascade.detectMultiScale(gray, 1.5, 4)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0,255,0))
     key = cv2.waitKey(10)
     if key == 27:
         break

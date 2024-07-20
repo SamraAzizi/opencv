0afb260cf020b64ba509f5bb18480402320cdc77
SamraAzizi <fahimaazizi369@gmail.com>
2024-07-20 17:35:49 +0500

    my commit

--- face_detect/main.py
+++ face_detect/main.py
@@ -1,6 +1,7 @@
 import cv2
 
-face_cascade = cv2.CascadeClassrifier('harrcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
 
 
 webcam = cv2.VideoCapture(0)

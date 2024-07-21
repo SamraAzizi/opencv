56d1e9a6db412c99894a7cc387dae96fe2af4804
SamraAzizi <fahimaazizi369@gmail.com>
2024-07-21 12:06:43 +0500

    my commit

--- HandGesture/hand_gesture.py
+++ HandGesture/hand_gesture.py
@@ -5,4 +5,11 @@ webcame = cv2.VideoCapture(0)
 while True:
     _, image = webcame.read()
     cv2.imshow("Hand Volume Control Using Python", image)
-    cv2.waitKey(10)
+    key = cv2.waitKey(10)
+
+    if key == 27:
+        break
+
+
+webcame.release()
+cv2.destroyAllWindows()

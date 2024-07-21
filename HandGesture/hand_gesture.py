e82b4089d0b3ada9b5e5aa2546fe71cccaed7a75
SamraAzizi <fahimaazizi369@gmail.com>
2024-07-21 12:21:48 +0500

    my commit

--- HandGesture/hand_gesture.py
+++ HandGesture/hand_gesture.py
@@ -2,9 +2,13 @@ import cv2
 
 webcame = cv2.VideoCapture(0)
 
+my_hands = mp.solutions.hans.Hands()
+drwaing_utils = mp.solutions.drawing_utlis
+
 while True:
     _, image = webcame.read()
     cv2.imshow("Hand Volume Control Using Python", image)
+    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     key = cv2.waitKey(10)
 
     if key == 27:

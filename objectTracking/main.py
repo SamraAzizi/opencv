28649609920cb09ccd696085ca30ea648ac98f8a
SamraAzizi <fahimaazizi369@gmail.com>
2024-07-24 15:28:13 +0500

    my commit

--- objectTracking/main.py
+++ objectTracking/main.py
@@ -12,7 +12,14 @@ while True:
     if not ret:
         break  # break the loop if the frame cannot be read
 
+    #object detection
+
     mask = object_detector.apply(frame)
+
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    for cnt in contours:
+        cv2.drawContours(frame, [cnt], -1, (0,255,0), 2)
 
     cv2.imshow("Frame", frame)
     cv2.imshow("Mask" ,mask)

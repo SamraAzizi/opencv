eb39008142cc44c13a36ea55b1ffe709f3ee4eff
SamraAzizi <fahimaazizi369@gmail.com>
2024-07-28 21:23:50 +0500

    background

--- backgroundRemoval/main.py
+++ backgroundRemoval/main.py
@@ -3,4 +3,12 @@ import cvzone
 from cvzone.SelfieSegmentationModule import SelfieSegmentation
 import os
 
-cap = cv2
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+while True:
+    success, img = cap.read()
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)

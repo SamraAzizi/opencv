49c454504239a849ec13eca2fbc4ea8d64c77ceb
SamraAzizi <fahimaazizi369@gmail.com>
2024-07-28 21:33:42 +0500

    background

--- backgroundRemoval/main.py
+++ backgroundRemoval/main.py
@@ -1,14 +1,17 @@
 import cv2
 import cvzone
-from cvzone.SelfieSegmentationModule import SelfieSegmentation
+from cvzone import SelfieSegmentation
 import os
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
+segmentor = SelfieSegmentation()
 
 while True:
     success, img = cap.read()
+    imgOut = segmentor.removeBG(img, (255,0,255))
 
     cv2.imshow("Image", img)
+    cv2.imshow("ImageOut", imgOut)
     cv2.waitKey(1)

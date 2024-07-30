4fbff30b4270e6a81ed421ab4bdd659c47018a97
SamraAzizi <fahimaazizi369@gmail.com>
2024-07-30 11:23:30 +0500

    Background

--- backgroundRemoval/main.py
+++ backgroundRemoval/main.py
@@ -6,6 +6,7 @@ import os
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
+cap.set(cv2.CAP_PROP_FPS, 60)
 segmentor = SelfieSegmentation()
 fpsReader = cvzone.FPS()
 
@@ -16,7 +17,7 @@ while True:
 
 
     imgStacked = cvzone.stackImages([img, imgOut], 2, 1)
-    fpsReader.update(img)
+    _, imgStacked = fpsReader.update(imgStacked, color=(0,0,255))
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)

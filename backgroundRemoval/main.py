fa029358a417c54fbc88f6451c1d0280a734566e
SamraAzizi <fahimaazizi369@gmail.com>
2024-07-30 11:06:10 +0500

    Resolve merge conflicts

--- backgroundRemoval/main.py
+++ backgroundRemoval/main.py
@@ -7,13 +7,16 @@ cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
 segmentor = SelfieSegmentation()
+fpsReader = cvzone.FPS()
 
 while True:
     success, img = cap.read()
     imgOut = segmentor.removeBG(img, (255,0,255), threshold = 0.8)
+    
+
 
     imgStacked = cvzone.stackImages([img, imgOut], 2, 1)
+    fpsReader.update(img)
 
     cv2.imshow("Image", img)
-    cv2.imshow("ImageOut", imgOut)
     cv2.waitKey(1)

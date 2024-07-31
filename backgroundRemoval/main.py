ae77e4e02772e6405e7f9352f6883b1530322abf
SamraAzizi <fahimaazizi369@gmail.com>
2024-07-31 12:36:21 +0500

    Background

--- backgroundRemoval/main.py
+++ backgroundRemoval/main.py
@@ -5,22 +5,23 @@ import os
 import numpy as np
 
 cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
-cap.set(cv2.CAP_PROP_FPS, 60)
-segmentor = mp.solutions.selfie_segmentation.SelfieSegmentation(model_selection = 1)
-fpsReader = cvzone.FPS()
 
 imgBg = cv2.imread("Images/2.jpg")
+segmentation = mp.solutions.selfie_segmentation.SelfieSegmentation(model_selection = 1)
+while cap.isOpened():
+    ret, frame = cap.read()
+    height, width, channel = frame.shape
+    RGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-while True:
+    results = segmentation.process(RGB)
+    mask = results.segmentationi_mask
     success, img = cap.read()
     imgOut = segmentor.removeBG(img, imgBg, threshold = 0.8)
     
 
 
     imgStacked = cvzone.stackImages([img, imgOut], 2, 1)
-    _, imgStacked = fpsReader.update(imgStacked, color=(0,0,255))
+    
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)

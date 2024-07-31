8307b4535d04798a83741eeef6ea95b39ba3252d
SamraAzizi <fahimaazizi369@gmail.com>
2024-07-31 19:56:07 +0500

    BackgroundRemoval

--- backgroundRemoval/main.py
+++ backgroundRemoval/main.py
@@ -1,23 +1,31 @@
 import cv2
-
+import cvzone
 import mediapipe as mp
-
+import os
 import numpy as np
 
 cap = cv2.VideoCapture(0)
 
-imgBg = cv2.imread("Images/2.jpg")
-segmentation = mp.solutions.selfie_segmentation.SelfieSegmentation(model_selection = 1)
+imgBg_path = "C:\\Users\\CPCM\\OneDrive\\Desktop\\opencv\\backgroundRemoval\\images\\hey.jpg"
+
+
+imgBg = cv2.imread(imgBg_path)
+
+
+segmentation = mp.solutions.selfie_segmentation.SelfieSegmentation(model_selection=1)
 
 while cap.isOpened():
     ret, frame = cap.read()
+    if not ret:
+        break
+
     height, width, channel = frame.shape
     RGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
     results = segmentation.process(RGB)
     mask = results.segmentation_mask
 
-    rsm = np.stack((mask, )*3, axis = -1)
+    rsm = np.stack((mask,) * 3, axis=-1)
 
     condition = rsm > 0.6
     condition = np.reshape(condition, (height, width, 3))

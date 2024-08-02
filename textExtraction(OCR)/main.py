1a10ebe0d8d2ee5616e428e1f8e460c357bf7b3d
SamraAzizi <fahimaazizi369@gmail.com>
2024-08-02 20:52:28 +0500

    BackgroundRemoval

--- textExtraction(OCR)/main.py
+++ textExtraction(OCR)/main.py
@@ -1,5 +1,9 @@
 import cv2
+import PIL.Image
+import pytesseract
 
 my_config = r"--psm 6 --oem 3"
 
-text = pytesseract.image_to_string(PIL.Image.open("txt.jpg"))
+text = pytesseract.image_to_string(PIL.Image.open("txt.jpg"), config=my_config)
+
+print(text)

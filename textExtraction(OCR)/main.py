ffd0c9fee4c3f7b59b32a3a149de9f2fa3a9bb8c
SamraAzizi <fahimaazizi369@gmail.com>
2024-08-03 12:49:25 +0500

    textExtraction

--- textExtraction(OCR)/main.py
+++ textExtraction(OCR)/main.py
@@ -7,7 +7,7 @@ from pytesseract import Output
 
 
 my_config = r"--psm 6 --oem 3"
-img = cv2.imread(r"C:\Users\CPCM\OneDrive\Desktop\opencv\textExtraction(OCR)\.jpg")
+img = cv2.imread(r"C:\Users\CPCM\OneDrive\Desktop\opencv\textExtraction(OCR)\logo.jpg")
 
 #if img is None:
  #   raise FileNotFoundError("Image file not found. Please check the path.")
@@ -27,10 +27,12 @@ data = pytesseract.image_to_data(img, config=my_config, output_type=Output.DICT 
 amount_of_boxes = len(data['text'])
 
 for i in range(amount_of_boxes):
-    if float(data['conf']) > 80:
-        (x, y, width, height) = (data['left'][i], data['top'][i], data['height'][i])
+    if float(data['conf'][i]) > 80:
+        (x, y, width, height) = (data['left'][i], data['top'][i],  data['width'][i], data['height'][i])
 
         img = cv2.rectangle(img, (x,y), (x+width, y + height), (0,255,0), 2)
+        img = cv2.putText(img, data['text'][i], (x,y+height + 20), cv2.FONT_HERSHEY_SIMPLEX, )
+
 cv2.imshow("Image", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()

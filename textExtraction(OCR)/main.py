2a1e52181393793c7ea5d89d6892584a07b42f11
SamraAzizi <fahimaazizi369@gmail.com>
2024-08-03 12:44:06 +0500

    textExtraction

--- textExtraction(OCR)/main.py
+++ textExtraction(OCR)/main.py
@@ -22,8 +22,13 @@ img = cv2.imread(r"C:\Users\CPCM\OneDrive\Desktop\opencv\textExtraction(OCR)\.jp
 
 
 
-data = pytesseract.image_to_data(img)
+data = pytesseract.image_to_data(img, config=my_config, output_type=Output.DICT )
 
+amount_of_boxes = len(data['text'])
+
+for i in range(amount_of_boxes):
+    if float(data['conf']) > 80:
+        (x, y, width, height) = (data['left'][i], data['top'][i], data['height'][i])
 cv2.imshow("Image", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()

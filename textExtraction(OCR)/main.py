ba1d3986b0e72064fcefe7d85c9caa44e98d6c44
SamraAzizi <fahimaazizi369@gmail.com>
2024-08-03 12:33:32 +0500

    textExtraction

--- textExtraction(OCR)/main.py
+++ textExtraction(OCR)/main.py
@@ -4,43 +4,21 @@ import pytesseract
 import os
 
 
-'''
-# Use the absolute path to the image file
-image_path = r"C:\Users\CPCM\OneDrive\Desktop\opencv\textExtraction(OCR)\text.jpg"
-
-# Check if the file exists
-if not os.path.exists(image_path):
-    raise FileNotFoundError(f"No such file: '{image_path}'")
-
-# Open the image file
-image = Image.open(image_path)
-
-# Specify Tesseract configuration
-my_config = r"--psm 6 --oem 3"
-
-# Perform OCR on the image
-text = pytesseract.image_to_string(image, config=my_config)
-
-# Print the extracted text
-print(text)
-
-
-
-
-
-##########################################
-'''
-
 
 my_config = r"--psm 6 --oem 3"
-img = cv2.imread("txt.jpg")
+img = cv2.imread(r"C:\Users\CPCM\OneDrive\Desktop\opencv\textExtraction(OCR)\txt.jpg")
 
-height, width, _ = img.shape()
+if img is None:
+    raise FileNotFoundError("Image file not found. Please check the path.")
+
+height, width, _ = img.shape
 
 boxes = pytesseract.image_to_boxes(img, config=my_config)
 
-
 for box in boxes.splitlines():
     box = box.split(" ")
-    
-    img = cv2.rectangle(img, (int(box[1]),  height - int(box[2])), (int(box[3]), height - int(box[4])),(0,255,0),2)
+    img = cv2.rectangle(img, (int(box[1]), height - int(box[2])), (int(box[3]), height - int(box[4])), (0, 255, 0), 2)
+
+cv2.imshow("Image", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()

12ed3ae187e584ec8ccd8b5bed8cbebe41e7776f
SamraAzizi <fahimaazizi369@gmail.com>
2024-08-03 12:29:21 +0500

    textExtraction

--- textExtraction(OCR)/main.py
+++ textExtraction(OCR)/main.py
@@ -38,4 +38,9 @@ img = cv2.imread("txt.jpg")
 height, width, _ = img.shape()
 
 boxes = pytesseract.image_to_boxes(img, config=my_config)
-print(boxes)
+
+
+for box in boxes.splitlines():
+    box = box.split(" ")
+    
+    img = cv2.rectangle(img, (int(box[1]),  height - int(box[2])), (int(box[3]), height - int(box[4])),(0,255,0),2)

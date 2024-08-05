375c8f25d72515d6df41f9d99f30e166be5a5bac
SamraAzizi <fahimaazizi369@gmail.com>
2024-08-05 21:10:04 +0500

    imageStitching

--- Image_Stitching/main.py
+++ Image_Stitching/main.py
@@ -13,4 +13,5 @@ for folder in myFolders:
 
     for imgN in myList:
         curImg = cv2.imread(f'{path}/{imgN}')
+        curImg = cv2.resize(curImg, (0,0), None, 0.2, 0.2)
         images.append(curImg)

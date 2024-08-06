bcb0b32295f0ce03af80eb5fae28b1bb3e8c75d8
Samra <118079600+SamraAzizi@users.noreply.github.com>
2024-08-06 20:51:38 +0500

    Update main.py

--- Image_Stitching/main.py
+++ Image_Stitching/main.py
@@ -4,7 +4,10 @@ import os
 
 mainFolder = "Images"
 myFolders = os.listdir(mainFolder)
-
+ for imgN in myList:
+        curImg = cv2.imread(f'{path}/{imgN}')
+        curImg = cv2.resize(curImg, (0,0), None, 0.2, 0.2)
+        images.append(curImg)
 
 for folder in myFolders:
     path = mainFolder + '/'+folder

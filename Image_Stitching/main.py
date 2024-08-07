fc6dfd1bc8669afccd7776e4380bec3df1d7b198
SamraAzizi <fahimaazizi369@gmail.com>
2024-08-07 19:36:47 +0500

    imageStitching

--- Image_Stitching/main.py
+++ Image_Stitching/main.py
@@ -18,24 +18,29 @@ if os.path.isdir(mainFolder):
         itemPath = os.path.join(mainFolder, item)
         if os.path.isfile(itemPath):
             print(f"Processing file: {itemPath}")
-           
+            # Add your file processing code here
+            
+            # Example: Assuming you are processing images
+            # You can replace this with your actual image processing logic
+            # For now, let's just print the file name
+            # (Do not use os.listdir on a file path)
         else:
             print(f"Skipping non-file item: {itemPath}")
 else:
     print(f"The path {mainFolder} is not a directory.")
 
 myFolders = os.listdir(mainFolder)
- for imgN in myList:
-        curImg = cv2.imread(f'{path}/{imgN}')
-        curImg = cv2.resize(curImg, (0,0), None, 0.2, 0.2)
-        images.append(curImg)
+
 
 for folder in myFolders:
     path = mainFolder + '/'+folder
     images = []
     myList = os.listdir(path)
 
-   
+    for imgN in myList:
+        curImg = cv2.imread(f'{path}/{imgN}')
+        curImg = cv2.resize(curImg, (0,0), None, 0.2, 0.2)
+        images.append(curImg)
 
 
     stitcher = cv2.Stitcher.create()

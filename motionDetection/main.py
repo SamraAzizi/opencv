73c5bfe80f1fe7b72c0315dd13e9006e68bb9cc8
SamraAzizi <fahimaazizi369@gmail.com>
2024-08-09 21:18:51 +0500

    imageStitching

--- motionDetection/main.py
+++ motionDetection/main.py
@@ -11,3 +11,5 @@ cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 _, start_frame = cap.read()
+start_frame = imutils.resize(start_frame, width = 500)
+start_frame = cv2.cvtColor(start_frame, cv2.COLOR_BGR2GRAY)

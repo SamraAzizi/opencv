b715931557e293ef320a4d2041a98c3654491403
SamraAzizi <fahimaazizi369@gmail.com>
2024-08-09 21:17:45 +0500

    imageStitching

--- motionDetection/main.py
+++ motionDetection/main.py
@@ -5,4 +5,9 @@ import winsound
 import threading
 
 
-cap = cv2
+cap = cv2.VideoCapture(0)
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+_, start_frame = cap.read()

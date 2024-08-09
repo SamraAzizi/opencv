be629bba8d39d0e322b8d363a7d5e3c34ac11f20
SamraAzizi <fahimaazizi369@gmail.com>
2024-08-09 21:20:53 +0500

    imageStitching

--- motionDetection/main.py
+++ motionDetection/main.py
@@ -13,3 +13,11 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 _, start_frame = cap.read()
 start_frame = imutils.resize(start_frame, width = 500)
 start_frame = cv2.cvtColor(start_frame, cv2.COLOR_BGR2GRAY)
+start_frame = cv2.GaussianBlur(start_frame, (21, 21), 0)
+
+alarm = False
+alarm_mode = False
+alarm_counter = 0
+
+def beep_alarm():
+    

e9c673d1db8b928ce3e2cb59cf30a7e78652e3a0
SamraAzizi <fahimaazizi369@gmail.com>
2024-08-12 21:19:25 +0500

    eyeDetection

--- motionDetection/eyeDetection/main.py
+++ motionDetection/eyeDetection/main.py
@@ -1,5 +1,6 @@
 import cv2
-import numpy as np
+import numpy as 
+import dlib
 
 cap = cv2.VideoCapture(0)
 
@@ -7,4 +8,9 @@ while True:
     _, frame = cap.read()
     cv2.imshow("frame", frame)
 
-    
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()

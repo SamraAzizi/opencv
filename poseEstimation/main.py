9f7f8077cbaa2db0e45cf901328c1e1b7e820711
SamraAzizi <fahimaazizi369@gmail.com>
2024-08-16 22:11:56 +0500

    poseEstimation

--- poseEstimation/main.py
+++ poseEstimation/main.py
@@ -4,10 +4,16 @@ import time
 
 
 cap = cv2.VideoCapture("")
+prevTime = 0
 
 while True:
     success, img = cap.read()
     cv2.imshow("Image", img)
 
-    currentTime = 
+    currentTime = time.time()
+    fps = 1/(currentTime - prevTime)
+    prevTime = currentTime
+
+    cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0),3)
+
     cv2.waitKey(1)

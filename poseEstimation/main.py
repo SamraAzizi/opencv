fc453b257c26207b1f9739d6b0b4a958df60997f
SamraAzizi <fahimaazizi369@gmail.com>
2024-08-17 11:59:07 +0500

    poseEstimation

--- poseEstimation/main.py
+++ poseEstimation/main.py
@@ -8,27 +8,33 @@ mpPose = mp.solutions.pose
 
 pose = mpPose.Pose()
 
-cap = cv2.VideoCapture("123.mp4")
+cap = cv2.VideoCapture(r"C:\Users\CPCM\OneDrive\Desktop\opencv\poseEstimation\123.mp4")
+
 prevTime = 0
 
 while True:
+
     success, img = cap.read()
 
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2GB)
+    if not success:
+        print("Failed to read frame. Exiting...")
+        break
+
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = pose.process(imgRGB)
     print(results.pose_landmarks)
     if results.pose_landmarks:
-        mpDraw.draw_landmarks()
-
-     
-    
+        mpDraw.draw_landmarks(img, results.pose_landmarks)
 
     currentTime = time.time()
     fps = 1/(currentTime - prevTime)
     prevTime = currentTime
 
-    cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0),3)
-                                
-                                
+    cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+
     cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()

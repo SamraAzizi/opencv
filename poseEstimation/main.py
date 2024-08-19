108376949325e007004db0aaca586dcde0b4e343
SamraAzizi <fahimaazizi369@gmail.com>
2024-08-19 20:34:36 +0500

    poseEstimation

--- poseEstimation/main.py
+++ poseEstimation/main.py
@@ -1,12 +1,11 @@
-
 import mediapipe as mp
 import time
 import cv2
 
 
-class poseDetector():
-    def __init__(self, mode = False, upperBody = False, smooth = True
-                 , detectionCon = 0.5, trackCon = 0.5):
+class PoseDetector():
+    def __init__(self, mode=False, upperBody=False, smooth=True, 
+                 detectionCon=0.5, trackCon=0.5):
         
         self.mode = mode
         self.upperBody = upperBody
@@ -17,26 +16,25 @@ class poseDetector():
         self.mpDraw = mp.solutions.drawing_utils
         self.mpPose = mp.solutions.pose
 
-        self.pose = self.mpPose.Pose(self.mode, self.upperBody, self.smooth
-                                     , self.detectionCon, self.trackCon)
+        self.pose = self.mpPose.Pose(static_image_mode=self.mode, 
+                                     model_complexity=self.upperBody, 
+                                     smooth_landmarks=self.smooth, 
+                                     min_detection_confidence=self.detectionCon, 
+                                     min_tracking_confidence=self.trackCon)
 
-    def findPose(self, img, draw = True):
+    def findPose(self, img, draw=True):
 
-    
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.pose.process(imgRGB)
         
-        
-
         if self.results.pose_landmarks:
             if draw:
                 self.mpDraw.draw_landmarks(img, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
                 
         return img
-    
 
 
-    def findPosition(self, img, draw = True):
+    def findPosition(self, img, draw=True):
         lmList = []
 
         if self.results.pose_landmarks:
@@ -57,24 +55,29 @@ def main():
     cap = cv2.VideoCapture(r"C:\Users\CPCM\OneDrive\Desktop\opencv\poseEstimation\123.mp4")
 
     prevTime = 0
-    detector = poseDetector()
+    detector = PoseDetector()
 
-while True:
+    while True:
 
-    success, img = cap.read()
-    img = detector.findPose(img)
-    lmList = detector.findPosition(img, draw=False)
-    cv2.circle(img, (lmList[1], lmList[2]), 5, (255, 0, 0), cv2.FILLED)
+        success, img = cap.read()
+        img = detector.findPose(img)
+        lmList = detector.findPosition(img, draw=False)
+        if lmList:
+            cv2.circle(img, (lmList[1][1], lmList[1][2]), 5, (255, 0, 0), cv2.FILLED)
 
-    currentTime = time.time()
-    fps = 1/(currentTime - prevTime)
-    prevTime = currentTime
+        currentTime = time.time()
+        fps = 1/(currentTime - prevTime)
+        prevTime = currentTime
 
-    cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+        cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
 
-    cv2.imshow("Image", img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        cv2.imshow("Image", img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()

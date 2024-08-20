9a3dad7d2e4960d7bcf41826b9d66a3c61cf10fd
SamraAzizi <fahimaazizi369@gmail.com>
2024-08-20 20:38:09 +0500

    poseEstimation

--- poseEstimation/awsomePose.py
+++ poseEstimation/awsomePose.py
@@ -16,18 +16,7 @@ while True:
     img = detector.findPose(img)
     lmList = detector.findPosition(img, draw=False)
     if lmList:
-        cv2.circle(img, (lmList[1][1], lmList[1][2]), 5, (255, 0, 0), cv2.FILLED)
-
-    currentTime = time.time()
-    fps = 1/(currentTime - prevTime)
-    prevTime = currentTime
-
-    cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
-
-    cv2.imshow("Image", img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
+       
 cap.release()
 cv2.destroyAllWindows()
 

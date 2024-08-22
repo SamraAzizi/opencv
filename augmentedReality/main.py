81030ce4c8e17a3dacfc38862dcbc293f9d2eedb
SamraAzizi <fahimaazizi369@gmail.com>
2024-08-22 11:58:10 +0500

    augmentedReality

--- augmentedReality/main.py
+++ augmentedReality/main.py
@@ -10,8 +10,12 @@ myVideo = cv2.VideoCapture()
 success , imgVideo = myVid.read()
 
 
+hT, wT, cT = imgTarget.shape
+imgVideo = cv2.resize(imgVideo, (wT, hT))
+
+
 cv2.imshow("ImageTarget", imgTarget)
-cv2.imshow("ImgTarget", imgVideo)
+cv2.imshow("myVideo", imgVideo)
 
 if cv2.waitKey(1) & 0xFF == ord('q'):
             break

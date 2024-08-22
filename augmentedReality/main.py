d01df1ff965478959f4d9155373114cd651fa4b6
SamraAzizi <fahimaazizi369@gmail.com>
2024-08-22 11:55:42 +0500

    augmentedReality

--- augmentedReality/main.py
+++ augmentedReality/main.py
@@ -3,3 +3,15 @@ import numpy as np
 
 
 cap = cv2.VideoCapture(0)
+
+imgTarget = cv2.imread("cards.jpg")
+myVideo = cv2.VideoCapture()
+
+success , imgVideo = myVid.read()
+
+
+cv2.imshow("ImageTarget", imgTarget)
+cv2.imshow("ImgTarget", imgVideo)
+
+if cv2.waitKey(1) & 0xFF == ord('q'):
+            break

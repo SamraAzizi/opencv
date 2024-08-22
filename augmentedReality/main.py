72bb7d2409fd348a0c5634fab7bf46b85f7afab5
SamraAzizi <fahimaazizi369@gmail.com>
2024-08-22 12:05:06 +0500

    augmentedReality

--- augmentedReality/main.py
+++ augmentedReality/main.py
@@ -12,13 +12,15 @@ success , imgVideo = myVid.read()
 
 hT, wT, cT = imgTarget.shape
 imgVideo = cv2.resize(imgVideo, (wT, hT))
-while Tue:
+while True:
         
-        successm imgWebcam = cap.read()
+        
+        success, imgWebcam = cap.read()
 
 
-cv2.imshow("ImageTarget", imgTarget)
-cv2.imshow("myVideo", imgVideo)
+        cv2.imshow("ImageTarget", imgTarget)
+        cv2.imshow("myVideo", imgVideo)
+        cv2.imshow("imgWebcam", imgWebcam)
 
-if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break

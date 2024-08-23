ac73489b809f46c8df61b63ed23f0d4333eb6339
SamraAzizi <fahimaazizi369@gmail.com>
2024-08-23 09:14:55 +0500

    augmentedReality

--- augmentedReality/main.py
+++ augmentedReality/main.py
@@ -12,6 +12,13 @@ success , imgVideo = myVid.read()
 
 hT, wT, cT = imgTarget.shape
 imgVideo = cv2.resize(imgVideo, (wT, hT))
+
+orb = cv2.ORB_create(nfeatures = 1000)
+
+kp1, des1 = orb.detectAndCompute(imgTarget, None)
+
+imgTarget = cv2.drawKeypoints(imgTarget, kp1, None)
+
 while True:
         
         

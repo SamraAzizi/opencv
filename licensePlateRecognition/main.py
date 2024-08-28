3ac677327fc73f56c79e800d334e7ce92ee9fbe1
SamraAzizi <fahimaazizi369@gmail.com>
2024-08-28 21:19:53 +0500

    licensePlateRecognition

--- licensePlateRecognition/main.py
+++ licensePlateRecognition/main.py
@@ -17,3 +17,7 @@ plt.imshow(cv2.cvtColor(edge, cv2.COLOR_BGR2RGB))
 keyPoints = cv2.findContours(edge.copy(), cv2.RETR_TREE, cv2.CHAIN_APROX_SIMPLE)
 contours = imutils.grab_contours(keyPoints)
 contours = sorted(contours, key=cv2.contourArea, reverse= True)[:10]
+
+location = None
+for contour in contours:
+    approx = cv2.approxPolyOP(contour, 10, True)

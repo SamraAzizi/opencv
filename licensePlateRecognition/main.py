6afdff8e0f489246cc00831ee3e6c4626b01f0b0
SamraAzizi <fahimaazizi369@gmail.com>
2024-08-30 16:04:21 +0500

    licensePlateRecognition

--- licensePlateRecognition/main.py
+++ licensePlateRecognition/main.py
@@ -4,48 +4,50 @@ import imutils
 import easyocr
 import numpy as np
 
-
+# Load the image
 img = cv2.imread('123.jpg')
+
+# Convert to grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 plt.imshow(cv2.cvtColor(gray, cv2.COLOR_BGR2RGB))
 
-
+# Apply a bilateral filter and edge detection
 bfilter = cv2.bilateralFilter(gray, 11, 17, 17)
 edge = cv2.Canny(bfilter, 30, 200)
 plt.imshow(cv2.cvtColor(edge, cv2.COLOR_BGR2RGB))
 
-
-keyPoints = cv2.findContours(edge.copy(), cv2.RETR_TREE, cv2.CHAIN_APROX_SIMPLE)
+# Find contours
+keyPoints = cv2.findContours(edge.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 contours = imutils.grab_contours(keyPoints)
-contours = sorted(contours, key=cv2.contourArea, reverse= True)[:10]
+contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
 
 location = None
 for contour in contours:
-    approx = cv2.approxPolyOP(contour, 10, True)
+    approx = cv2.approxPolyDP(contour, 10, True)
     if len(approx) == 4:
         location = approx
         break
 
-
+# Create a mask and segment the region of interest
 mask = np.zeros(gray.shape, np.uint8)
-new_image = cv2.drawContours(mask, [location], 0,255,-1)
-new_image = cv2.bitwise_and(img, img, mask = mask)
+new_image = cv2.drawContours(mask, [location], 0, 255, -1)
+new_image = cv2.bitwise_and(img, img, mask=mask)
 plt.imshow(cv2.cvtColor(new_image, cv2.COLOR_BGR2RGB))
 
-
-(x,y) = np.where(mask == 255)
-(x1,y1) = (np.min(x), np.min(y))
+# Crop the region of interest
+(x, y) = np.where(mask == 255)
+(x1, y1) = (np.min(x), np.min(y))
 (x2, y2) = (np.max(x), np.max(y))
 cropped_image = gray[x1:x2+1, y1:y2+1]
-
 plt.imshow(cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB))
 
+# Perform OCR to extract text
 reader = easyocr.Reader(['en'])
 result = reader.readtext(cropped_image)
 
-
+# Display the result
 text = result[0][-2]
 font = cv2.FONT_HERSHEY_SIMPLEX
-res = cv2.putText(img, text = text, org=(approx[0][0][0], approx[1][0][1]+60), fontFace= font, fontScale = 1, color=(0,255,0), thickness= 2, lineTypes= cv2.LINE_AA)
-res = cv2.rectangle(img, tuple(approx[0][0]), tuple(approx[2][0]), (0,255,0), 3)
+res = cv2.putText(img, text=text, org=(approx[0][0][0], approx[1][0][1]+60), fontFace=font, fontScale=1, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
+res = cv2.rectangle(img, tuple(approx[0][0]), tuple(approx[2][0]), (0, 255, 0), 3)
 plt.imshow(cv2.cvtColor(res, cv2.COLOR_BGR2RGB))

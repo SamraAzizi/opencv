62c2ffeda7cf8128562df12683710249f06ff446
SamraAzizi <fahimaazizi369@gmail.com>
2024-08-30 15:34:27 +0500

    licensePlateRecognition

--- licensePlateRecognition/main.py
+++ licensePlateRecognition/main.py
@@ -47,3 +47,4 @@ result = reader.readtext(cropped_image)
 text = result[0][-2]
 font = cv2.FONT_HERSHEY_SIMPLEX
 res = cv2.putText(img, text = text, org=(approx[0][0][0], approx[1][0][1]+60), fontFace= font, fontScale = 1, color=(0,255,0), thickness= 2, lineTypes= cv2.LINE_AA)
+res = cv2.rectangle(img, tuple(approx[0][0]), tuple(approx[2][0]), (0,255,0), 3)

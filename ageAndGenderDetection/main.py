692171676a6552cdbe1c74ae30cb511f5d2e7567
SamraAzizi <fahimaazizi369@gmail.com>
2024-09-03 20:46:10 +0500

    ageAndGenderDetection

--- ageAndGenderDetection/main.py
+++ ageAndGenderDetection/main.py
@@ -2,3 +2,9 @@ import cv2
 
 image = cv2.imread("123.webp")
 image = cv2.resize(image, (720, 640))
+
+
+#define modules
+ 
+face_pbtxt = "models/opencv_face_detector.pbtxt"
+face_pb = "models/opencv_face_detector_uint8.pb"

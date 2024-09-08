f2616fb64776fc6cedd044de7e296718c5f701dc
SamraAzizi <fahimaazizi369@gmail.com>
2024-09-08 19:58:26 +0500

    emotionDetection

--- emotionDetection/main.py
+++ emotionDetection/main.py
@@ -10,3 +10,4 @@ from keras.preprocessing.image import load_img, img_to_array
 from keras.preprocessing.image import ImageDataGenerator
 from keras.layers import Dense, Input, Dropout, GlobalAveragePooling2D, Flatten, Conv2D, BatchNormalization, Activation, MaxPooling2
 from keras.model import Model, Sequentail
+from keras.optimizers import Adam, SGD, RMprop

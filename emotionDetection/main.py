32147773abfd17909197d91b44ccbeb7a45a2c3a
SamraAzizi <fahimaazizi369@gmail.com>
2024-09-08 19:54:47 +0500

    emotionDetection

--- emotionDetection/main.py
+++ emotionDetection/main.py
@@ -2,3 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as np
 import seaborn as sns
+import os
+
+# importing Deep Learning Libraries
+
+from keras.preprocessing.image import load_img, img_to_array

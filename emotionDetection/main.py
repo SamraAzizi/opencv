26b2408ea8d26e132895c5794f35ba99b6a4f16e
SamraAzizi <fahimaazizi369@gmail.com>
2024-09-10 21:19:25 +0500

    emotionDetection

--- emotionDetection/main.py
+++ emotionDetection/main.py
@@ -9,3 +9,7 @@ import numpy as np
 face_classifier = cv2.CascadeClassifier(r'C:\Users\CPCM\OneDrive\Desktop\opencv\emotionDetection\haarcascade_frontalface_default.xml')
 
 classifier = load_model(r'C:\Users\CPCM\OneDrive\Desktop\opencv\emotionDetection\model.h5')
+
+emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy','Neutral', 'Sad', 'Surprise']
+
+cap = cv2.VideoCapture(0)

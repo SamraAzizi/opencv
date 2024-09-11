76a1f4d2337e359681380659bfb17316b9317dc6
SamraAzizi <fahimaazizi369@gmail.com>
2024-09-11 10:57:08 +0500

    emotionDetection

--- emotionDetection/main.py
+++ emotionDetection/main.py
@@ -30,6 +30,10 @@ while True:
         if np.sum([roi_gray])!= 0:
             roi = roi_gray.astype('float')/255.0
             roi = img_to_array(roi)
+            roi = np.expand_dims(roi, axis=0)
+            
+
+            prediction = classifier.predict(roi)[0]
                   
 
     

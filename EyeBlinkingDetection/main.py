f57db2fbb3c5f5a0c06d396c44360d2cf5bf60f5
SamraAzizi <fahimaazizi369@gmail.com>
2024-09-16 21:00:56 +0500

    eyeBlinkingDetection

--- EyeBlinkingDetection/main.py
+++ EyeBlinkingDetection/main.py
@@ -30,6 +30,8 @@ def get_blinkingRatio(eye_points, facial_landmarks ):
         
     ratio = hor_line_lenght / ver_line_lenght
 
+    return ratio
+
 while True:
     _, frame = cap.read()
 
@@ -43,21 +45,7 @@ while True:
        # cv2.rectangle(frame, (x,y), (x1, y1), (0, 255, 0), 2)
 
         landmarks = predictor(gray, face)
-        left_point = (landmarks.part(36).x, landmarks.part(36).y)
-        right_point = (landmarks.part(39).x, landmarks.part(39).y)
-
-        center_top = midpoint(landmarks.part(37), landmarks.part(38))
-        center_bottom = midpoint(landmarks.part(41), landmarks.part(40))
-
-        horizontal_line = cv2.line(frame, left_point, right_point, (0, 255, 0), 2)
-        ver_line = cv2.line(frame,center_top, center_bottom, (0,255, 0) ,2)
-
-
-        hor_line_lenght = hypot((left_point[0] - right_point[0]), (left_point[1] - right_point[1]))
-        ver_line_lenght = hypot((center_top[0] - center_bottom[0]), center_top[1] - center_bottom[1])
-
         
-        ratio = hor_line_lenght / ver_line_lenght
 
         if ratio > 5.7:
             cv2.putText(frame, "BLINKING", (50, 150), font, 7, (255, 0, 0))

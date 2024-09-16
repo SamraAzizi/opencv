3699431fca68d2874dcf49c4179075ce58267fd2
SamraAzizi <fahimaazizi369@gmail.com>
2024-09-16 20:38:45 +0500

    eyeBlinkingDetection

--- EyeBlinkingDetection/main.py
+++ EyeBlinkingDetection/main.py
@@ -13,6 +13,23 @@ def midpoint(p1, p2):
 
 font = cv2.FONT_HERSHEY_PLAIN
 
+def get_blinkingRatio(points, ):
+    left_point = (landmarks.part(36).x, landmarks.part(36).y)
+    right_point = (landmarks.part(39).x, landmarks.part(39).y)
+
+    center_top = midpoint(landmarks.part(37), landmarks.part(38))
+    center_bottom = midpoint(landmarks.part(41), landmarks.part(40))
+
+    horizontal_line = cv2.line(frame, left_point, right_point, (0, 255, 0), 2)
+    ver_line = cv2.line(frame,center_top, center_bottom, (0,255, 0) ,2)
+
+
+    hor_line_lenght = hypot((left_point[0] - right_point[0]), (left_point[1] - right_point[1]))
+    ver_line_lenght = hypot((center_top[0] - center_bottom[0]), center_top[1] - center_bottom[1])
+
+        
+    ratio = hor_line_lenght / ver_line_lenght
+
 while True:
     _, frame = cap.read()
 

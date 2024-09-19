fb0d90e0cc61c8701a14fbcb58b1e56c068c4a7e
SamraAzizi <fahimaazizi369@gmail.com>
2024-09-19 11:18:59 +0500

    colorDetection

--- colorDetection/main.py
+++ colorDetection/main.py
@@ -4,4 +4,9 @@ import cv2
 def get_limits(color):
 
     c = np.uint8([[color]]) # here insert the bgr values which you want to convert
-    hsvC = cv2
+    hsvC = cv2.cvtColor(c,cv2.COLOR_BGR2HSV)
+
+lowerLimit = hsvC[0][0][0] - 10, 100, 100
+upperLimit = hsvC[0][0][0] + 10, 255, 255
+
+lowerLimit - np.array(lowerLimit, dtype=np.uint8)

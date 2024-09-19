e202a633ea341f24a8dddd3674992af83fb625d8
SamraAzizi <fahimaazizi369@gmail.com>
2024-09-19 11:29:46 +0500

    colorDetection

--- colorDetection/main.py
+++ colorDetection/main.py
@@ -6,6 +6,9 @@ cap = cv2.VideoCapture(2)
 while True:
     ret, frame, = cap.read()
 
+    cv2.cvtColor(frame , cv2.COLORBGR2HSV)
+
+
     cv2.imshow('frame', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
